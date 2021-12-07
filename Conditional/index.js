835563d4949b970b0c0c9f530d72363a21fdd4ac
yashintaaulia <yashintaaulia97@gmail.com>
2021-12-07 13:01:19 +0700

    day 2

---
+++ Conditional/index.js
@@ -0,0 +1,108 @@
+// var angka = "sepuluh";
+// if (angka === "sepuluh") {
+//     console.log("angka sepuluh");
+// } else {
+//     console.log("angka selain seuluh");
+// }
+
+// var beverages = "tea";
+// var nama = "Yashinta";
+// var result = nama + ", kamu pilih ";
+
+// switch (beverages) {
+//     case "coffee":
+//         result = result + beverages;
+//         break;
+//     case "tea":
+//         result = result + beverages;
+//         break;
+// }
+// // console.log(result);
+
+// var angka = 5;
+
+// if(angka % 2 === 0) {
+//     console.log(angka + " adalah Genap")
+// }else{
+// //     console.log(angka + " adalah Ganjil")
+// // }
+
+// var berat = 75;
+
+// if(berat > 100){
+//     console.log("obesitas")
+// }else if(berat <= 100 && berat > 80){
+//     console.log("gemuk")
+// }else if(berat <= 80 && berat > 65){
+//     console.log("ideal")
+// }else if(berat <= 65 && berat > 50){
+//     console.log("kurus")
+// }else if(berat <= 50 && berat >= 0){
+//     console.log("sangat kurus")
+// }else{
+//     console.log("Masukkan berat yang benar. Trims")
+// }
+
+// var book = "yyy";
+// var total = 100;
+// var name = "Yashinta"
+// var totalBayar;
+
+// switch (book.toLowerCase()){
+//     case "coding":
+//         if (total >= 50000) {
+//             totalBayar = total - (total * 0.1)
+//         } else {
+//             totalBayar = total - (total * 0.05)
+//         }
+//         console.log(name + ", membayar Rp. " + totalBayar);
+//         break;
+//     case "cooking":
+//         if (total >= 50000) {
+//             totalBayar = total - (total * 0.05)
+//         }else {
+//             totalBayar = total - (total * 0.025)
+//         }
+//         console.log(name + ", membayar Rp. " + totalBayar);
+//         break;
+//     default:
+//         console.log("Masukkan buku yang benar, trims")
+// }
+
+var level = 7;
+var access = "public"
+
+switch (access.toLowerCase()) {
+    case "":
+        console.log("Input Access")
+        break;
+    case "publik":
+        if (level < 5) {
+            console.log("Public di bawah 5")
+        } else if (level > 5) {
+            console.log("Public di atas 5")
+        } else if (level == 5) {
+            console.log("Public five")
+        }
+        break;
+    case "private":
+        if (level <5) {
+            console.log("Private di bawah 5")
+        } else if (level > 5) {
+            console.log("Private di atas 5")
+        } else if (level == 5) {
+            console.log("Private five")
+        }
+        break;
+    case "protected":
+        if (level <5) {
+            console.log("Protected di bawah 5")
+        } else if (level > 5) {
+            console.log("Protected di atas 5")
+        } else if (level == 5) {
+            console.log("Protected five")
+        }
+        break;
+    default:
+        console.log("Access is not defined")
+}
