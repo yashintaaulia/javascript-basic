b15a0ad0cb4e2d6b2f605dbf950e39863973ad41
yashintaaulia <yashintaaulia97@gmail.com>
2021-12-07 15:08:33 +0700

    day 2 pol

--- Conditional/index.js
+++ Conditional/index.js
@@ -65,44 +65,79 @@
 //         }
 //         console.log(name + ", membayar Rp. " + totalBayar);
 //         break;
+// //     default:
+// //         console.log("Masukkan buku yang benar, trims")
+// // }
+
+// var level = 7;
+// var access = "public"
+
+// switch (access.toLowerCase()) {
+//     case "":
+//         console.log("Input Access")
+//         break;
+//     case "publik":
+//         if (level < 5) {
+//             console.log("Public di bawah 5")
+//         } else if (level > 5) {
+//             console.log("Public di atas 5")
+//         } else if (level == 5) {
+//             console.log("Public five")
+//         }
+//         break;
+//     case "private":
+//         if (level <5) {
+//             console.log("Private di bawah 5")
+//         } else if (level > 5) {
+//             console.log("Private di atas 5")
+//         } else if (level == 5) {
+//             console.log("Private five")
+//         }
+//         break;
+//     case "protected":
+//         if (level <5) {
+//             console.log("Protected di bawah 5")
+//         } else if (level > 5) {
+//             console.log("Protected di atas 5")
+//         } else if (level == 5) {
+//             console.log("Protected five")
+//         }
+//         break;
 //     default:
-//         console.log("Masukkan buku yang benar, trims")
+//         console.log("Access is not defined")
+// // }
+// for (var i = 1; i < 11; i++) {
+//     if(i % 2 === 0) {
+//         console.log(i + " adalah Genap")
+//     } else {
+//         console.log(i + " adalah Ganjil")
+//     }
 // }
+// var str = "sayur bayam";
+// for (var i = 0 ; i < str.length; i++) {
+//     console.log(str[i].toUpperCase())
+// }
+// var n = 5;
+// var temp = '';
 
-var level = 7;
-var access = "public"
+// for(var i =1; i <=n; i++){
+//     temp = temp + i;
+// }
+// console.log(temp);
 
-switch (access.toLowerCase()) {
-    case "":
-        console.log("Input Access")
-        break;
-    case "publik":
-        if (level < 5) {
-            console.log("Public di bawah 5")
-        } else if (level > 5) {
-            console.log("Public di atas 5")
-        } else if (level == 5) {
-            console.log("Public five")
+var n = 5;
+
+if (n % 2 === 1) {
+    var temp = '';
+
+    for (var i = 0; i < n; i++){
+        if (i % 2 === 0) {
+            temp += "#"
+    } else {
+            temp += "@"
         }
-        break;
-    case "private":
-        if (level <5) {
-            console.log("Private di bawah 5")
-        } else if (level > 5) {
-            console.log("Private di atas 5")
-        } else if (level == 5) {
-            console.log("Private five")
-        }
-        break;
-    case "protected":
-        if (level <5) {
-            console.log("Protected di bawah 5")
-        } else if (level > 5) {
-            console.log("Protected di atas 5")
-        } else if (level == 5) {
-            console.log("Protected five")
-        }
-        break;
-    default:
-        console.log("Access is not defined")
+    }
+    console.log(temp);
+} else {
+    console.log("Angka harus ganjil")
 }
